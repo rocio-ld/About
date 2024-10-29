253376914344c00c1fd1d67238e9cc92a550880e
rocio_ld <rocio_ld@hotmail.com>
2024-10-29 13:21:42 +0100

    header ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import About from "./About";
 import Footer from "./Footer";
 import Header from "./Header";
 import Secret from "./Secret";
-import Title from "./Title";
+
 
 
 function App() {
@@ -13,8 +13,7 @@ function App() {
   return (
     <>
    
-        <Header/>
-        <Title/>
+        <Header/>  
         <About/>
         <Secret/>
         <Footer/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,19 @@ function Header() {
   return (
     
   <header className="header">
-    <h1 className="header__titulo">HOLA, SOY ROCÍO LÓPEZ</h1>
-    <nav>
+    <h1 className="header__title">HOLA, SOY ROCÍO LÓPEZ</h1>
+    <nav className="header__nav">
       <ul className="header__list">
         <li>SOBRE MÍ</li>
         <li>MI HISTORIA</li>
         <li>CONTACTO</li>
       </ul>
      </nav>
-  <img 
-    src={picture} 
-    alt="Imagen de Presentación de Rocío López"
-    title="Imagen de Presentación de Rocío López"
-    className="header__img"/>
-  
+  <div className="header__div">
+    <img className="header__div--img"src={picture} alt="Imagen Presentación Rocío López"
+    title="Imagen de Presentación de Rocío López" />
+    <h2 className="header__div--h2">Haciendo del Código una Herramienta para mejorar mi Vida</h2>
+  </div>
   </header>
   )
 }
