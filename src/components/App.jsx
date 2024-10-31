b69bd5237e38ce2aa1bafa56201edd70bce7ab2f
rocio_ld <rocio_ld@hotmail.com>
2024-10-31 13:23:52 +0100

    guardar proyectos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,52 @@
+import React, {useState} from "react";
 import About from "./About";
 import Home from "./Home";
 import Contact from "./Contact";
 import History from "./History";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route,} from "react-router-dom";
+import anime from "../images/PAGINA-WEB-ANIME.png";
+import canino from "../images/PAGINA-WEB-CANINO-NATURAL.png";
+import videojuego from "../images/VIDEOJUEGO-CUIDADO-CON-GROGU.png";
+import ProjectItem from "./ProjectItem";
+
 
 
 function App() {
+
   
+  //useState para ampliar y reducir tamaño de proyectos
+
+  const [hoveredProject, setHoveredProject]= useState (null);
+
+  const projects = [
+    { id: 1, title: "PAGÍNA WEB ANIME", img: anime },
+    { id: 2, title: "PAGÍNA WEB CANINO NATURAL", img: canino },
+    { id: 3, title: "VIDEOJUEGO CUIDADO CON GROGU", img: videojuego },
+  ];
+ 
+  // actualiza el estado al proyecto hovered
+  const handleMouseEnter =(id)=>{
+    setHoveredProject(id);
+  };
+  //Resetea el estado al salir del hover
+  const handleMouseLeave=()=>{
+    setHoveredProject(null);
+  };
   return (
     
    <>
+   
     <Routes>
-      <Route path="/"element={<Home/>  }/>
+      <Route path="/"element={<Home 
+              handleMouseEnter={handleMouseEnter}
+              handleMouseLeave={handleMouseLeave}
+              hoveredProject={hoveredProject}
+            />  }/>
       <Route path="/about"element={<About/>  }/>
       <Route path="/history"element={<History/>  }/>
       <Route path="/contact"element={<Contact/> }/>
     </Routes>
+    
     </>
   )
 }
---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,22 @@
+import "../scss/ProjectItem.scss";
+
+function ProjectItem({handleMouseEnter, handleMouseLeave, hoveredProject, projects }) {
+  return (
+    <section className="sectionProject">
+    <h2 className="sectionProject__h2">PROYECTOS DESTACADOS</h2>
+      <div className="sectionProject__div">
+    {projects.slice(0, 3).map(project => (
+    <div key={project.id}  
+    className={`sectionProject__item ${hoveredProject === project.id ? "hovered" : ""}`}
+    onMouseEnter={() => handleMouseEnter(project.id)}
+    onMouseLeave={handleMouseLeave}>
+      <h3 className="sectionProject__div--title">{project.title}</h3>
+      <img src={project.img} alt={project.title} className="sectionProject__div--img"/>
+</div>
+))} 
+    </div>
+  </section>
+  )
+}
+
+export default ProjectItem
