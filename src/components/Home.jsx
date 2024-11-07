3d4d2cbc9e29fec81c4037032f1ff3309eaa3f82
rocio_ld <rocio_ld@hotmail.com>
2024-11-07 11:58:03 +0100

    guardar array project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,31 @@ import { Link } from "react-router-dom";
 import picture from "../images/foto-rocioLopez.jpg";
 import Cv from "./Cv";
 import Footer from "./Footer";
-
+import github from "../images/github.png";
+import js from "../images/js.png";
+import css3 from "../images/CSS3.png";
+import html5 from "../images/HTML5.png";
+import react from "../images/React.svg";
 
 function Home({handleProjectClick, hoveredProject}) {
 
   const projects = [
-    { id: 1, title: "PAGÍNA WEB ANIME", img: anime  },
-    { id: 2, title: "PAGÍNA WEB CANINO NATURAL", img: canino  },
-    { id: 3, title: "VIDEOJUEGO CUIDADO CON GROGU", img: videojuego  },
+    { id: 1, 
+      title: "PAGÍNA WEB ANIME", 
+      img: anime , 
+      desc: "Página web alimentada de API, dónde podemos hacer búsquedas de películas Anime, seleccionar tu película favorita o eliminarla de la lista.", tecn: 
+      [github,js,css3, html5]
+    },
+    { id: 2, 
+      title: "PAGÍNA WEB CANINO NATURAL", 
+      img: canino , 
+      desc: "Página web con diseño responsive alimentada de API externa, de  alimentación de perros, dónde puedes rellenar en un formulario con datos y dichos datos se renderizará en una tarjeta.", 
+      tecn: [github,js,css3, html5] 
+    },
+    { id: 3, 
+      title: "VIDEOJUEGO CUIDADO CON GROGU", img: videojuego , 
+      desc: "Videojuego que consiste en tirar el dado de forma aleatoria, y Grogu según el número aleatorio avanzará casillas o tu como jugador irás vaciando mercancía. ", tecn: [github,js,css3, html5, react] 
+    },
   ];
 
   return (
